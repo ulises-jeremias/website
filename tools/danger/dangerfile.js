d13d09fcb307cac1d04538efea1b6f35f4299aaf
ulises-jeremias <17727170+ulises-jeremias@users.noreply.github.com>
2026-08-30 23:44:19 +0000

    [MegaLinter] Apply linters fixes

--- tools/danger/dangerfile.js
+++ tools/danger/dangerfile.js
@@ -1,9 +1,9 @@
-const { danger, fail, message, warn } = require('danger');
+const { danger, fail, message, warn } = require('danger')
 
-const SMALL_PR_FILES = 10;
-const SMALL_PR_LINES = 200;
+const SMALL_PR_FILES = 10
+const SMALL_PR_LINES = 200
 
-const templateSections = ['## Description', '## Type of Change', '## How Has This Been Tested?', '## Checklist'];
+const templateSections = ['## Description', '## Type of Change', '## How Has This Been Tested?', '## Checklist']
 
 const checklistItems = [
   'My code follows the style guidelines of this project',
@@ -12,52 +12,52 @@ const checklistItems = [
   'I have made corresponding changes to the documentation',
   'My changes generate no new warnings',
   'Any dependent changes have been merged and published in downstream modules',
-  'I have checked my code and corrected any misspellings',
-];
+  'I have checked my code and corrected any misspellings'
+]
 
 if (!danger.github.pr.body) {
-  fail(':clipboard: Missing Summary - Please add a `## Description` section to your PR description.');
+  fail(':clipboard: Missing Summary - Please add a `## Description` section to your PR description.')
 }
 if (!danger.github.pr.title) {
-  fail(':id: Missing PR Title - Please add a title.');
+  fail(':id: Missing PR Title - Please add a title.')
 }
 // Bot-authored PRs (Dependabot, ImgBot, Renovate, ...) never fill the human
 // PR template - skip template/checklist policing for them. Content lints
 // below still run for everyone.
-const isBotAuthored = /([bot]$|^app\/)/.test(danger.github.pr.user.login || '');
-const hasSection = (section) => danger.github.pr.body.includes(section);
-const isChecklistItemChecked = (item) => danger.github.pr.body.includes(`- [x] ${item}`);
+const isBotAuthored = /([bot]$|^app\/)/.test(danger.github.pr.user.login || '')
+const hasSection = (section) => danger.github.pr.body.includes(section)
+const isChecklistItemChecked = (item) => danger.github.pr.body.includes(`- [x] ${item}`)
 if (!isBotAuthored) {
   templateSections.forEach((section) => {
     if (!hasSection(section)) {
-      fail(`:clipboard: Missing Section - Please include the section: <i>${section}</i> in your PR description.`);
+      fail(`:clipboard: Missing Section - Please include the section: <i>${section}</i> in your PR description.`)
     }
-  });
+  })
   checklistItems.forEach((item) => {
     if (!isChecklistItemChecked(item)) {
-      warn(`:clipboard: Unchecked Checklist Item - Please check the item: <i>${item}</i> in your PR description.`);
+      warn(`:clipboard: Unchecked Checklist Item - Please check the item: <i>${item}</i> in your PR description.`)
     }
-  });
+  })
 }
-const touchedFiles = danger.git.created_files.concat(danger.git.modified_files);
-const allFiles = touchedFiles.concat(danger.git.deleted_files);
-const diffsList = Promise.all(allFiles.map((p) => danger.git.diffForFile(p)));
+const touchedFiles = danger.git.created_files.concat(danger.git.modified_files)
+const allFiles = touchedFiles.concat(danger.git.deleted_files)
+const diffsList = Promise.all(allFiles.map((p) => danger.git.diffForFile(p)))
 diffsList
   .then((diffs) => diffs.filter(Boolean))
   .then((diffs) => ({
     removed: diffs.reduce((lines, diff) => lines + diff.removed.split('\n').length, 0),
     added: diffs.reduce((lines, diff) => lines + diff.added.split('\n').length, 0),
     lines: diffs.reduce((lines, diff) => lines + diff.added.split('\n').length + diff.removed.split('\n').length, 0),
-    files: diffs.length,
+    files: diffs.length
   }))
   .then((diff) => {
     if (diff.added < diff.removed) {
-      message('Thanks! We :heart: removing more lines than added!');
+      message('Thanks! We :heart: removing more lines than added!')
     }
     if (diff.lines <= SMALL_PR_LINES && diff.files <= SMALL_PR_FILES) {
-      message('Thanks! We :heart: small PRs!');
+      message('Thanks! We :heart: small PRs!')
     }
     if (diff.lines > SMALL_PR_LINES) {
-      warn(`This PR is changing more than ${SMALL_PR_LINES} lines.`);
+      warn(`This PR is changing more than ${SMALL_PR_LINES} lines.`)
     }
-  });
+  })
